0b30375d8db071c50664ef44f364beb4e411d4d0
acernst <aernst1586@gmail.com>
2015-05-28 06:06:26 -0400

    LatestRevision_5_28

--- MinnesotaSampleLottery/Program.cs
+++ MinnesotaSampleLottery/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MinnesotaSampleLottery.DAL;
+using MinnesotaSampleLottery.Common;
 
 namespace MinnesotaSampleLottery
 {
@@ -10,6 +12,47 @@ namespace MinnesotaSampleLottery
     {
         static void Main(string[] args)
         {
+            SampleApplication();
+        }
+
+        public static void SampleApplication()
+        {
+            start:
+            Menu.MenuDisplay();
+
+            GameCollection showGames = GameDAL.GetCollection();
+
+            foreach (Game display in showGames)
+                System.Console.WriteLine(display.GameId + " " + display.Name);
+
+            System.Console.WriteLine("Enter one of the game Id's above to view their details");
+
+            int input = System.Console.ReadLine().ToInt();
+            System.Console.WriteLine(" ");
+
+            Game details = GameDAL.GetItem(input);
+
+            System.Console.WriteLine("Selected: " + details.Name);
+            System.Console.WriteLine("Number Drawn: " + details.NumbersDrawn);
+            System.Console.WriteLine("Multiplier: " + details.Multiplier);
+            System.Console.WriteLine("Game Details: " + details.Description);
+            
+            System.Console.WriteLine("\nPress 1 to restart or press 2 to end program");
+
+            int nextStep = System.Console.ReadLine().ToInt();
+            switch (nextStep)
+            {
+                case 1:
+                    System.Console.Clear();
+                    goto start;
+                    break;
+
+                case 2:
+                    goto end;
+                    break;
+            }
+
+        end: ;
         }
     }
 }
---
+++ MinnesotaSampleLottery/SubSections/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinnesotaSampleLottery
+{
+    public class Menu
+    {
+        static void ShowMenu(string[] args)
+        {
+            MenuDisplay();
+        }
+
+        public static void MenuDisplay()
+        {
+            System.Console.WriteLine("================================================ \n\n" + 
+                                     "   Welcome to the Minnesota Lottery Application \n\n"
+                                     + "================================================");
+            System.Console.WriteLine("\nPress any key to continue");
+            System.Console.ReadLine();
+        }
+    }
+}
